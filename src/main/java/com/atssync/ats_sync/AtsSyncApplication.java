3506894ea475f183759d0cb1d6f26688619ecffc
paradox4994 <ryanpereira499@gmail.com>
2025-10-04 02:29:06 -0400

    Initial Commit

---
+++ src/main/java/com/atssync/ats_sync/AtsSyncApplication.java
@@ -0,0 +1,13 @@
+package com.atssync.ats_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atssync/ats_sync/AtsSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atssync.ats_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
