a1e74eab7d12ee19e20ef3a317bbe1cb2ec80516
paradox4994 <ryanpereira499@gmail.com>
2025-11-10 19:17:48 -0500

    Update server dependencies and modify hello endpoint response

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+  res.send('Hello ATS-Sync!');
 });
 
 
