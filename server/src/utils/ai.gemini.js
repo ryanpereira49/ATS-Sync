c1c6da4361f82a4cc927ccb415afa3e34b2eba3e
paradox4994 <ryanpereira499@gmail.com>
2025-11-13 12:51:51 -0500

    refactor: improve error handling and structure response in ExtractDataFromResume function

--- server/src/utils/ai.gemini.js
+++ server/src/utils/ai.gemini.js
@@ -1,12 +1,19 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-try{
-    const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite' });
-    const result = await model.generateContent(prompt);
+const ExtractDataFromResume = async (resumeText) => {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+    const result = await model.generateContent(resumeText + "\n Structure the key candidate information in JSON format.");
     const response = await result.response;
     const text = response.text();
-} catch(err){
+    return text;
+  } catch (err) {
     console.error("Error initializing Gemini API:", err);
-}
+  }
+};
+
+module.exports = {
+  ExtractDataFromResume,
+};
