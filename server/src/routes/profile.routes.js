796aa20c6ca4e9b7e9e4a285e9b4a68f07ed745c
paradox4994 <ryanpereira499@gmail.com>
2025-11-13 11:51:37 -0500

    feat: add route for uploading resumes with multer integration

--- server/src/routes/profile.routes.js
+++ server/src/routes/profile.routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { upload } from "../utils/multer.config.js";
 import { profileController } from '../controllers/profile.controller.js';
 
 const router = express.Router();
@@ -8,5 +9,7 @@ router.get('/:userId', profileController.getProfile);
 router.post('/:userId', profileController.createProfile);
 router.put('/:userId', profileController.updateProfile);
 router.delete('/:userId', profileController.deleteProfile);
+
+router.post("/upload-resume", upload.single("resume"), profileController.uploadResume);
 
 export default router;
